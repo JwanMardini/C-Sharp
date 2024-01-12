c15189b03506eace4edf22b6928151ca89608e90
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2024-01-12 01:51:13 +0100

    First lesson in karlstad

---
+++ Karlstad/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int answer = random.Next(0, 1000);
+            int score = 0;
+            Console.WriteLine(answer);
+            while (true)
+            {
+                Console.Write("Guess: ");
+                string text = Console.ReadLine();
+                int guess = int.Parse(text);
+
+                if (answer == guess)
+                {
+                    Console.WriteLine($"Correct!, {score}");
+                    return;
+                }
+                else if (answer < guess)
+                {
+                    Console.WriteLine("Too large");
+                }
+                else
+                {
+                    Console.WriteLine("Too small");
+                }
+                score++;
+            }
+        }
+    }
+}
---
+++ Karlstad/MyFirstApplication/MyFirstApplication/Program.cs
@@ -0,0 +1,93 @@
+﻿// variables
+/*int number = 10;
+
+number = 20;
+number = number + 50;
+
+Console.WriteLine(number);
+
+double number2 = 100.1;
+Console.WriteLine(number2);
+
+string text = "Hello World";
+text = text + " from Jwan";
+Console.WriteLine(text);
+
+bool isValid = false;
+Console.WriteLine(isValid);
+
+var test = 10; // It will be declared in compliation time
+var test2 = "Hello";
+Console.WriteLine(test);
+Console.WriteLine(test2);*/
+
+// Konvertering
+/*string text = "50";
+int tal = int.Parse(text);
+Console.WriteLine(tal);
+
+int tal2 = 50;
+string text2 = tal2.ToString();
+Console.WriteLine(text2);
+
+bool test = false;
+string text3 = test.ToString();
+Console.WriteLine(text3);
+
+string text4 = "50.225";
+double tal3 = double.Parse(text4);
+Console.WriteLine(tal3);
+
+int tal4 = 50;
+double test2 = tal4;
+Console.WriteLine(test2);
+
+double tal5 = 50.5;
+int test3 = (int) tal5;
+Console.WriteLine(test3);*/
+
+//if else
+/*Console.Write("Temperature: ");
+string text = Console.ReadLine();
+int temperature = int.Parse(text);
+if (temperature > 20)
+{
+    Console.WriteLine("It is warm");
+    if (temperature > 30)
+    {
+        Console.WriteLine("Very hot");
+    }
+}
+else if(temperature > 10)
+{
+    Console.WriteLine("Not sure");
+}
+else
+{
+    Console.WriteLine("it is cold");
+}*/
+
+// switch case
+/*int status = 0;
+
+switch (status)
+{
+    case 0: Console.WriteLine("0"); break;
+    case 1: Console.WriteLine("1"); break;
+    case 2: Console.WriteLine("2"); break;
+}*/
+
+//Iteration
+/*int counter = 0;
+while (counter < 5)
+{
+    Console.WriteLine($"Hello {counter + 1}");
+    counter++;
+}
+Console.WriteLine("done");
+Console.WriteLine();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"HELLO {i +1}");
+}*/
