f208c80efce06565167c294f0d50f560354533f0
Jwan Mardini <98626283+JwanMardini@users.noreply.github.com>
2024-01-15 14:08:32 +0100

    Exercise1 Malmö done

---
+++ Malmö/ShoppingBasketApplication/ShoppingBasketApplication/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasketApplication
+{
+    internal class Product
+    {
+        private String name;
+        private double price;
+        private int quantity;
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public String getName() {return this.name;}
+
+        public double getPrice() { return this.price;}
+
+        public int getQuantity() { return this.quantity;}
+
+        public void printReceipt()
+        {
+            Console.WriteLine("The number of this prooduct is 9\n");
+            Console.WriteLine("***** Receipt *****\n");
+            Console.WriteLine($"Product      :      {this.name}");
+            Console.WriteLine($"Unit price   :      {this.price}");
+            Console.WriteLine($"Quantity     :      {this.quantity}\n");
+
+            Console.WriteLine($"Amount to pay:      {this.quantity * this.price}\n");
+            Console.WriteLine(" **** Welcome back! ****");
+        }
+
+
+    }
+}
---
+++ Malmö/ShoppingBasketApplication/ShoppingBasketApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ShoppingBasketApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Welcome to my shop!");
+            Console.WriteLine("Name of the product?");
+            String name = Console.ReadLine();
+            Console.WriteLine($"Thanks {name}!");
+            Console.WriteLine("Price?");
+            double price;
+            while (true)
+            {
+                try{
+                    String priceInput = Console.ReadLine();
+                    price = double.Parse(priceInput);
+                    break;
+                }
+                catch (Exception){
+                    Console.WriteLine("Something went wrong, try again!");
+                }
+            }
+            /*try/catch is slow, we can do it this way for parseing:
+            int price;
+            bool success = int.TryParse(priceInput, out price)
+            if(!success){
+            //Error
+            }*/
+            Console.WriteLine($"The price of {name} is {price}");
+            Console.WriteLine("Quantity of the product?");
+            int quantity;
+            while (true)
+            {
+                try
+                {
+                    String quantityInput = Console.ReadLine();
+                    quantity = int.Parse(quantityInput);
+                    break;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Something went wrong, try again!");
+                }
+            }
+            Product product = new Product(name, price, quantity);
+            product.printReceipt();
+        }
+    }
+}
